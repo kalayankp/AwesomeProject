bebc2b225ed95a07eee1286f1b321533152e7237
kalayankp <kalyan.sabado@gmail.com>
2023-07-14 12:31:46 +0530

    completed

--- App.tsx
+++ App.tsx
@@ -1,15 +1,33 @@
-import { Text, View } from 'react-native'
-import React, { Component } from 'react'
-
+import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import StackNavigator from './src/TabNavigator.js/StackNavigator';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function App() {
 
-    return (
+
+
+const Stack = createNativeStackNavigator();
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import Home from './src/Home';
+
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: 'https://countries.trevorblades.com/graphql',
+  cache: new InMemoryCache()
+});
+
+export default function App() {
+  return (
+    <ApolloProvider client={client}>
     <NavigationContainer>
-   <StackNavigator/>
-   </NavigationContainer>
-    )
-  }
-export default App;
+      <Stack.Navigator>
+        
+        <Stack.Screen name="Home" component={Home} options={{ title: 'Welcome' }} />
+        
+
+      </Stack.Navigator>
+    </NavigationContainer>
+    </ApolloProvider>
+
+  );
+}
